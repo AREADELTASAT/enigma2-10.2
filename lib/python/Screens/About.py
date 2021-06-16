f47bacbc5df4fcebf0119b6f6406ba3b53906c3d
areadeltasat <50342699+AREADELTASAT@users.noreply.github.com>
2021-06-16 05:27:42 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -32,10 +32,11 @@ class About(Screen):
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
+		AboutText += _("Compiled by areadelta: ") + about.getBuildDateString() + "\n"
 		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
-		AboutText += _("Compiled by areadelta") + about.getUpdateDateString() + "\n"
+		AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
 
 		EnigmaVersion = about.getEnigmaVersionString()
 		EnigmaVersion = EnigmaVersion.rsplit("-", EnigmaVersion.count("-") - 2)
