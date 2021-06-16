bc4e00d3682db97e63c6a8966abf233651d96129
areadeltasat <50342699+AREADELTASAT@users.noreply.github.com>
2021-06-16 21:02:38 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -34,7 +34,7 @@ class About(Screen):
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
 		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
 		"\n"
-                Open Delta Image by AREADELTASAT Team "\n"
+                AboutText += _("Open Delta Image by AREADELTASAT Team") "\n"
 		"\n"
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
